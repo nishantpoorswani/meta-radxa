7b3035063fd618fbca41650b5cca84e27229958e
nishantpoorswani <nishantpoorswani@gmail.com>
2020-08-12 16:58:05 +0530

    Update boot.cmd to use the updated partitioning scheme
    The partitioning scheme has changed so boot.cmd must match it.
    Credit: Pantelis Antoniou <pantelis.antoniou@konsulko.com>
    
    Signed-off-by: nishantpoorswani <nishantpoorswani@gmail.com>

--- recipes-bsp/u-boot/files/rockpi-s/boot.cmd
+++ recipes-bsp/u-boot/files/rockpi-s/boot.cmd
@@ -14,8 +14,8 @@ setenv docker_optimizations "off"
 
 echo "Boot script loaded from ${devtype} ${devnum}"
 
-if test -e ${devtype} ${devnum}:4 ${prefix}uEnv.txt; then
-	load ${devtype} ${devnum}:4 ${load_addr} ${prefix}uEnv.txt
+if test -e ${devtype} ${devnum}:1 ${prefix}uEnv.txt; then
+	load ${devtype} ${devnum}:1 ${load_addr} ${prefix}uEnv.txt
 	env import -t ${load_addr} ${filesize}
 fi
 
@@ -23,33 +23,33 @@ setenv bootargs "console=tty1 console=${console} rw root=PARTUUID=${rootuuid} ro
 
 if test "${docker_optimizations}" = "on"; then setenv bootargs "${bootargs} cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory swapaccount=1"; fi
 
-load ${devtype} ${devnum}:4 ${kernel_addr_r} ${prefix}${kernelimg}
+load ${devtype} ${devnum}:1 ${kernel_addr_r} ${prefix}${kernelimg}
 
-load ${devtype} ${devnum}:4 ${fdt_addr_r} ${prefix}${fdtfile}
+load ${devtype} ${devnum}:1 ${fdt_addr_r} ${prefix}${fdtfile}
 fdt addr ${fdt_addr_r}
 fdt resize 65536
 for overlay_file in ${overlays}; do
-	if load ${devtype} ${devnum}:4 ${load_addr} ${prefix}overlays/${overlay_file}.dtbo; then
+	if load ${devtype} ${devnum}:1 ${load_addr} ${prefix}overlays/${overlay_file}.dtbo; then
 		echo "Applying kernel provided DT overlay ${overlay_file}.dtbo"
 		fdt apply ${load_addr} || setenv overlay_error "true"
 	fi
 done
 for overlay_file in ${user_overlays}; do
-	if load ${devtype} ${devnum}:4 ${load_addr} ${prefix}overlay-user/${overlay_file}.dtbo; then
+	if load ${devtype} ${devnum}:1 ${load_addr} ${prefix}overlay-user/${overlay_file}.dtbo; then
 		echo "Applying user provided DT overlay ${overlay_file}.dtbo"
 		fdt apply ${load_addr} || setenv overlay_error "true"
 	fi
 done
 if test "${overlay_error}" = "true"; then
 	echo "Error applying DT overlays, restoring original DT"
-	load ${devtype} ${devnum}:4 ${fdt_addr_r} ${prefix}${fdtfile}
+	load ${devtype} ${devnum}:1 ${fdt_addr_r} ${prefix}${fdtfile}
 else
-	if load ${devtype} ${devnum}:4 ${load_addr} ${prefix}overlays/${overlay_prefix}-fixup.scr; then
+	if load ${devtype} ${devnum}:1 ${load_addr} ${prefix}overlays/${overlay_prefix}-fixup.scr; then
 		echo "Applying kernel provided DT fixup script (${overlay_prefix}-fixup.scr)"
 		source ${load_addr}
 	fi
-	if test -e ${devtype} ${devnum}:4 ${prefix}overlay-user/fixup.scr; then
-		load ${devtype} ${devnum}:4 ${load_addr} ${prefix}overlay-user/fixup.scr
+	if test -e ${devtype} ${devnum}:1 ${prefix}overlay-user/fixup.scr; then
+		load ${devtype} ${devnum}:1 ${load_addr} ${prefix}overlay-user/fixup.scr
 		echo "Applying user provided fixup script (overlay-user/fixup.scr)"
 		source ${load_addr}
 	fi
